bcb8e4ffa3d0e3fedf24867b85c21638163e6977
myjasonvhs <myjasonvhs@163.com>
2017-03-10 19:29:43 +0800

    book-shop

---
+++ public/admin/js/content.js
@@ -0,0 +1,96 @@
+// var $parentNode = window.parent.document;
+
+// function $childNode(name) {
+//     return window.frames[name]
+// }
+
+// // tooltips
+// $('.tooltip-demo').tooltip({
+//     selector: "[data-toggle=tooltip]",
+//     container: "body"
+// });
+
+// // 使用animation.css修改Bootstrap Modal
+// $('.modal').appendTo("body");
+
+// $("[data-toggle=popover]").popover();
+
+
+//判断当前页面是否在iframe中
+if (top == this) {
+    var gohome = '<div class="gohome"><a class="animated bounceInUp" href="/admin/main" title="返回首页"><i class="fa fa-home"></i></a></div>';
+    $('body').append(gohome);
+}
+
+//animation.css
+function animationHover(element, animation) {
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);
+        },
+        function() {
+            //动画完成之前移除class
+            window.setTimeout(function() {
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        });
+}
+
+//拖动面板
+function WinMove() {
+    var element = "[class*=col]";
+    var handle = ".ibox-title";
+    var connect = "[class*=col]";
+    $(element).sortable({
+            handle: handle,
+            connectWith: connect,
+            tolerance: 'pointer',
+            forcePlaceholderSize: true,
+            opacity: 0.8,
+        })
+        .disableSelection();
+};
+
+//默认的验证插件配置信息
+$.validator.setDefaults({
+        errorElement: "em",
+        errorPlacement: function(error, element) {
+            // Add the `help-block` class to the error element
+            error.addClass("help-block");
+
+            // Add `has-feedback` class to the parent div.form-group
+            // in order to add icons to inputs
+            element.parents(".col-sm-10").addClass("has-feedback");
+
+            if (element.prop("type") === "checkbox") {
+                error.insertAfter(element.parent("label"));
+            } else {
+                error.insertAfter(element);
+            }
+
+            // Add the span element, if doesn't exists, and apply the icon classes to it.
+            if (!element.next("span")[0]) {
+                $("<span class='glyphicon glyphicon-remove form-control-feedback'></span>").insertAfter(element);
+            }
+        },
+        success: function(label, element) {
+            // Add the span element, if doesn't exists, and apply the icon classes to it.
+            if (!$(element).next("span")[0]) {
+                $("<span class='glyphicon glyphicon-ok form-control-feedback'></span>").insertAfter($(element));
+            }
+        },
+        highlight: function(element, errorClass, validClass) {
+            $(element).parents(".col-sm-10").addClass("has-error").removeClass("has-success");
+            $(element).next("span").addClass("glyphicon-remove").removeClass("glyphicon-ok");
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).parents(".col-sm-10").addClass("has-success").removeClass("has-error");
+            $(element).next("span").addClass("glyphicon-ok").removeClass("glyphicon-remove");
+        }
+    })
+//添加手机号码验证
+$.validator.addMethod('isMobile', function(val, ele) {
+    var regMobile = /^1[3857]{1}\d{9}$/
+    return regMobile.test(val)
+})
---
+++ routes/api/v1/books.js
@@ -0,0 +1,76 @@
+var express = require('express')
+var router = express.Router()
+var BookDal = require('../../../common/book').BookDal
+var bookDal = new BookDal()
+
+//无条件的返回数据
+router.get('/',(req,res)=>{
+    //bookDal.getData({type:req.query.type},data=>{
+    //    // res.json({status:'y',msg:'获取数据成功',data:data})
+    //    res.json(data)
+    //})
+    var page = 1//页码
+    if(req.query.page){
+        page = Number(req.query.page)
+    }
+    bookDal.getDataByPage(page,{type:req.query.type},data=>{
+        res.json(data.res)
+    })
+})
+
+//获取分页数量
+router.get('/get_page_count',(req,res)=>{
+    var page = 1//页码
+    if(req.query.page){
+        page = Number(req.query.page)
+    }
+    bookDal.getDataByPage(page,{type:req.query.type},data=>{
+        res.json(data)
+    })
+})
+
+//根据id获取单条记录
+// /api/v1/books/....
+router.get('/:id',(req,res)=>{
+    bookDal.findByID(req.params.id,data=>{
+        // data = data.toJSON()
+        // delete data.__v
+        // res.json({status:'y',msg:'获取数据成功',data:data})
+        res.json(data)
+    })
+})
+//根据id更新一条记录
+router.put('/:id',(req,res)=>{
+    console.log(req.body)
+    delete req.body._id //删除不需要修改的数据
+    delete req.body.__v	//删除不需要修改的数据
+    bookDal.updateByID(req.params.id,req.body,isOk=>{
+        // if(isOk){
+        // 	res.json({status:'y',msg:'修改数据成功',data:req.body})
+        // }
+        res.json(req.body)
+    })
+})
+//新增记录
+router.post('/',(req,res)=>{
+    // 判断当前传递过来的数据是否有type属性 如果没有为其添加一个type为other
+    if(req.body.type){
+    }
+    else{
+        req.body.type = 'other'
+    }
+    bookDal.save(req.body,(isOK,data)=>{
+        if(isOK){
+            // res.json({status:'y',msg:'新增数据成功',data:data})
+            res.json(data)
+        }
+    })
+})
+//根据id删除记录
+router.delete('/:id',(req,res)=>{
+    bookDal.del(req.params.id,isOK=>{
+        // res.json({status:'y',msg:'删除数据成功',data:{}})
+        res.json({})
+    })
+})
+module.exports = router
